542273043fbd2a2547fe32ae707a1e90e632f575
musetea <dev0019@naver.com>
2021-09-04 22:01:11 +0900

    v

--- client/src/marketO.ts
+++ client/src/marketO.ts
@@ -1,6 +1,29 @@
 class MarketIO {
-   constructor() {
 
+   private $menuBtn: HTMLDivElement;
+   private $navBar: HTMLElement;
+   constructor() {
+      this.$menuBtn = document.querySelector("#menu-btn")! as HTMLDivElement;
+      this.$navBar = document.querySelector("header nav.navbar")! as HTMLElement;
+
+      this.$menuBtn.addEventListener("click", this.OnMenuClick);
+
+      window.onscroll = () => {
+         this.removeToggleMenu();
+      }
+   }
+
+
+   // 메뉴클릭 
+   private OnMenuClick = (e: MouseEvent) => {
+      e.preventDefault();
+      this.$menuBtn.classList.toggle("fa-times");
+      this.$navBar.classList.toggle("active");
+   }
+
+   private removeToggleMenu = () => {
+      this.$menuBtn.classList.remove("fa-times");
+      this.$navBar.classList.remove("active");
    }
 }
 
--- public/js/marketO.js
+++ public/js/marketO.js
@@ -1,6 +1,22 @@
 "use strict";
 class MarketIO {
     constructor() {
+        // 메뉴클릭 
+        this.OnMenuClick = (e) => {
+            e.preventDefault();
+            this.$menuBtn.classList.toggle("fa-times");
+            this.$navBar.classList.toggle("active");
+        };
+        this.removeToggleMenu = () => {
+            this.$menuBtn.classList.remove("fa-times");
+            this.$navBar.classList.remove("active");
+        };
+        this.$menuBtn = document.querySelector("#menu-btn");
+        this.$navBar = document.querySelector("header nav.navbar");
+        this.$menuBtn.addEventListener("click", this.OnMenuClick);
+        window.onscroll = () => {
+            this.removeToggleMenu();
+        };
     }
 }
 new MarketIO();
