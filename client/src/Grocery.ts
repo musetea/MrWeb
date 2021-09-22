74717beb9276110e346d7101e7c4c8f9b46b102b
musetea <dev0019@naver.com>
2021-09-22 12:43:22 +0900

    commit

--- client/src/Grocery.ts
+++ client/src/Grocery.ts
@@ -1,11 +1,16 @@
 class Grocery {
     private $searchForm: SearchForm;
+    private $shoppingCart: ShoppingCart;
 
     constructor() {
 
         const _searchForm = document.querySelector("#search-form")! as HTMLFormElement;
         this.$searchForm = new SearchForm(_searchForm);
 
+        const _cart = document.querySelector(".header .shopping-cart")! as HTMLDivElement;
+        this.$shoppingCart = new ShoppingCart(_cart);
+
+        const _loginForm = document.querySelector("#login-form")! as HTMLFormElement;
     }
 };
 
@@ -15,9 +20,47 @@ class SearchForm {
         this.$btn = document.querySelector("#search-btn")! as HTMLDivElement;
 
         this.$btn.addEventListener("click", this.onSearchClick);
+
+        onscroll = () => {
+            this._form.classList.remove("active");
+            this.$btn.classList.remove("active");
+        }
     }
 
     private onSearchClick = () => {
+        this._form.classList.toggle("active");
+        this.$btn.classList.toggle("active");
+    }
+};
+
+class ShoppingCart {
+    private $btn: HTMLDivElement;
+    constructor(private _cart: HTMLDivElement) {
+        this.$btn = document.querySelector("#cart-btn")! as HTMLDivElement;
+        this.$btn.addEventListener("click", this.onShoppingCartClick);
+    }
+
+    private onShoppingCartClick = () => {
+        this._cart.classList.toggle("active");
+        this.$btn.classList.toggle("active");
+    }
+
+    onscroll = () => {
+        this._cart.classList.remove("active");
+        this.$btn.classList.remove("active");
+    }
+}
+
+// 로그인폼 
+class LoginForm {
+    private $btn: HTMLDivElement;
+
+    constructor(private _form: HTMLFormElement) {
+        this.$btn = document.querySelector("#login-btn")! as HTMLDivElement;
+        this.$btn.addEventListener("click", this.onLoginClick);
+
+    }
+    private onLoginClick = () => {
         this._form.classList.toggle("active");
         this.$btn.classList.toggle("active");
     }
--- public/js/Grocery.js
+++ public/js/Grocery.js
@@ -3,6 +3,9 @@ class Grocery {
     constructor() {
         const _searchForm = document.querySelector("#search-form");
         this.$searchForm = new SearchForm(_searchForm);
+        const _cart = document.querySelector(".header .shopping-cart");
+        this.$shoppingCart = new ShoppingCart(_cart);
+        const _loginForm = document.querySelector("#login-form");
     }
 }
 ;
@@ -15,6 +18,38 @@ class SearchForm {
         };
         this.$btn = document.querySelector("#search-btn");
         this.$btn.addEventListener("click", this.onSearchClick);
+        onscroll = () => {
+            this._form.classList.remove("active");
+            this.$btn.classList.remove("active");
+        };
+    }
+}
+;
+class ShoppingCart {
+    constructor(_cart) {
+        this._cart = _cart;
+        this.onShoppingCartClick = () => {
+            this._cart.classList.toggle("active");
+            this.$btn.classList.toggle("active");
+        };
+        this.onscroll = () => {
+            this._cart.classList.remove("active");
+            this.$btn.classList.remove("active");
+        };
+        this.$btn = document.querySelector("#cart-btn");
+        this.$btn.addEventListener("click", this.onShoppingCartClick);
+    }
+}
+// 로그인폼 
+class LoginForm {
+    constructor(_form) {
+        this._form = _form;
+        this.onLoginClick = () => {
+            this._form.classList.toggle("active");
+            this.$btn.classList.toggle("active");
+        };
+        this.$btn = document.querySelector("#login-btn");
+        this.$btn.addEventListener("click", this.onLoginClick);
     }
 }
 new Grocery();
